f281b8686af4c1af7923dd28596fdea1d3b96789
kalpaj12 <kalpaj.agrawalla01@gmail.com>
2019-02-27 17:18:33 +0530

    Beautify Output and indent shell script

--- .vscode/Builder.sh
+++ .vscode/Builder.sh
@@ -7,28 +7,24 @@ then
         ./a.out < $1/input$counter > $1/myoutput$counter
         cp $1/myoutput$counter $1/temp_myoutput
         cp $1/output$counter $1/temp_output
-
         sed -i -e 's/[\t \n]//g;/^$/d' $1/temp_output
         sed -i -e 's/[\t \n]//g;/^$/d' $1/temp_myoutput
-        
         diff -w $1/temp_myoutput $1/temp_output > /dev/null
         error=$?
-        
         if [ $error -eq 0 ]
         then
-            echo "Testcase# $counter  :AC"
-            echo '\n'            
+            echo "Testcase #$counter: AC"
             rm $1/myoutput$counter
         elif [ $error -eq 1 ]
         then
-            echo "Testcase# $counter  :WA"
-            echo "Expected"
+            echo "Testcase #$counter: ==WA=="
+            echo "Expected:"
             cat "$1/output$counter"
-            echo "Output"
+            echo "Output:"
             cat "$1/myoutput$counter"
-            echo '\n'
         fi
         counter=$((counter+1))
+        echo '\n'
     done
     rm a.out
     rm $1/temp_myoutput
